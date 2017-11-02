924fce4f9251a6f035246c1e94f90cd875bcd9f1
zhanghan13 <zhanghan13@meituan.com>
2017-11-02 15:00:24 +0800

    Spark Hash Partitioner

--- core/src/main/java/com/sdu/spark/Partitioner.java
+++ core/src/main/java/com/sdu/spark/Partitioner.java
@@ -2,6 +2,9 @@ package com.sdu.spark;
 
 import java.io.Serializable;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.sdu.spark.utils.Utils.nonNegativeMod;
+
 /**
  * @author hanhan.zhang
  * */
@@ -10,4 +13,28 @@ public abstract class Partitioner implements Serializable {
     public abstract int numPartitions();
     public abstract int getPartition(Object key);
 
+
+    public static class HashPartitioner extends Partitioner {
+
+        private int partitions;
+
+        public HashPartitioner(int partitions) {
+            checkArgument(partitions > 0, String.format("Number of partitions cannot be negative but found %d", partitions));
+            this.partitions = partitions;
+        }
+
+        @Override
+        public int numPartitions() {
+            return partitions;
+        }
+
+        @Override
+        public int getPartition(Object key) {
+            if (key == null) {
+                return 0;
+            }
+            return nonNegativeMod(key.hashCode(), partitions);
+        }
+
+    }
 }
